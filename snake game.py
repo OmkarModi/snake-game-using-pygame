b442da3aef8ebca8382e877f046116c10cf99c21
OmkarModi <omkarmodi5859@gmail.com>
2020-11-29 01:20:46 +0530

    added score and length increase

--- snake game.py
+++ snake game.py
@@ -2,6 +2,13 @@ import pygame as pg
 import random
 import time
 
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
 pg.init()
 dis_width = 800
 dis_height  = 600
@@ -9,11 +16,21 @@ dis=pg.display.set_mode((dis_width ,dis_height))
 pg.display.update()
 pg.display.set_caption("snake game")
 
+font_style = pg.font.SysFont("bahnschrift", 25)
+score_font = pg.font.SysFont("comicsansms", 35)
+
 snake_block = 10
-clock = pg.time.Clock()
 snake_speed = 30
 
-font_style = pg.font.SysFont(None, 50)
+clock = pg.time.Clock()
+
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pg.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
 def message(msg,color):
     mesg = font_style.render(msg, True, color)
@@ -29,6 +46,9 @@ def gameLoop():  # creating a function
     x1_change = 0
     y1_change = 0
 
+    snake_List = []
+    Length_of_snake = 1
+
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
 
@@ -38,6 +58,7 @@ def gameLoop():  # creating a function
         while game_close == True:
                 dis.fill((255,255,255))
                 message("You Lost!  Press Q-Quit or C-Play Again", (255,0,0))
+                Your_score(Length_of_snake - 1)
                 pg.display.update()
 
                 for event in pg.event.get():
@@ -73,11 +94,31 @@ def gameLoop():  # creating a function
 
         x1 += x1_change
         y1 += y1_change
-        dis.fill([255,255,255])
-        pg.draw.rect(dis,(0,0,0),[x1,y1,snake_block,snake_block])
-        pg.draw.rect(dis, (0,0,255), [foodx, foody, snake_block, snake_block])
+        dis.fill(blue)
+        pg.draw.rect(dis, green , [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:           #if snake bites itself
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
         pg.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
         clock.tick(snake_speed)
+
 
     pg.quit()
     quit()
